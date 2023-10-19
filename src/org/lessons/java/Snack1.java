736d1d8bba5dfc4b0ca705522e30bad875231526
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-19 15:15:39 +0200

    commit 1 - Snack1 add condition and comment

--- src/org/lessons/java/Snack1.java
+++ src/org/lessons/java/Snack1.java
@@ -1,4 +1,9 @@
 package org.lessons.java;
+/*
+Snack1
+Continua a chiedere all’utente di inserire un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+Se il numero è negativo termina.
+ */
 
 import java.util.Scanner;
 
@@ -9,11 +14,17 @@ public class Snack1 {
         while (true) {
             System.out.print("Insert one number: ");
             int number = scan.nextInt();
-            if (number % 2 == 0) {
+            if  (number < 0) {
+                System.out.println("You entered a negative number. The program ends.");
+                break;
+            }
+            else if (number % 2 == 0){
                 System.out.println("the number is: " + number);
-            }else {
+            }
+            else {
                 System.out.println("the number is: " + (number + 1));
             }
+
         }
 
     }
