3d77c77b9bb84f8a0996641a066e6523d3b745e6
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-19 17:10:34 +0200

    commit 4 - Snack4

--- src/org/lessons/java/Snack3.java
+++ src/org/lessons/java/Snack3.java
@@ -5,7 +5,6 @@ Snack3
 Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari;
  */
 
-
 public class Snack3 {
     public static void main(String[] args) {
         int[] numbers = new int[20];
--- src/org/lessons/java/Snack4.java
+++ src/org/lessons/java/Snack4.java
@@ -1,7 +1,36 @@
 package org.lessons.java;
 
+/*
+Snack4
+Data in input una stringa verificare se è palindroma
+*/
+
+import java.util.Scanner;
+
 public class Snack4 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.print("First word: ");
+        String firstWord = scan.nextLine();
+        //System.out.println("You wrote " + word.toUpperCase());
+
+        String word = firstWord.toLowerCase();
+
+        char[] invertedChars = new char[word.length()];
+        for (int i = word.length() - 1, j = 0; i >= 0 && j < word.length(); i--, j++) {
+            invertedChars[j] = word.charAt(i);
+        }
+        String invertedWord = String.valueOf(invertedChars);
+
+        //System.out.print("The reversed word is: " + invertedWord);
+
+        if (invertedWord.equals(word)){
+            System.out.print("\nThe word is palindrome");
+        }else {
+            System.out.print("\nThe word " + firstWord +  " is not a palindrome");
+        }
+
+        scan.close();
     }
 }
