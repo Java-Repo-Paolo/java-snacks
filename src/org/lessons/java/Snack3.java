0180c444f299cc2bfe146f7cf13ad76dbaebc5b5
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-19 16:36:29 +0200

    commit 3 - Snack3

--- src/org/lessons/java/Snack3.java
+++ src/org/lessons/java/Snack3.java
@@ -1,7 +1,25 @@
 package org.lessons.java;
 
+/*
+Snack3
+Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari;
+ */
+
+
 public class Snack3 {
     public static void main(String[] args) {
+        int[] numbers = new int[20];
+        int sumPosOdd = 0;
 
+        System.out.println("The numbers in odd position are: ");
+        for (int i = 0; i < 20; i++) {
+            numbers[i] = i + 1;
+            //  System.out.print(i + " ");
+            if (i % 2 != 0){
+                System.out.print(numbers[i] + " ");
+                sumPosOdd += numbers[i];
+            }
+        }
+        System.out.println("\nThe sum of the numbers in odd position is: " + sumPosOdd);
     }
 }
