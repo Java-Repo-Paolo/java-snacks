637de9d117b16e854b7c965bbeba0e110f92ca0a
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-19 15:10:38 +0200

    commit 1 - Snack1

--- src/org/lessons/java/Snack1.java
+++ src/org/lessons/java/Snack1.java
@@ -1,7 +1,20 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class Snack1 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Insert one number: ");
+            int number = scan.nextInt();
+            if (number % 2 == 0) {
+                System.out.println("the number is: " + number);
+            }else {
+                System.out.println("the number is: " + (number + 1));
+            }
+        }
 
     }
 }
